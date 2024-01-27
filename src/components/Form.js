e750cbcefe0212fefcb3c97a6188f678dba974d6
sree0611 <sreejith0611@gmail.com>
2024-01-27 12:27:00 +0530

    Added my project

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,71 +1,89 @@
-import React,{useState, useEffect} from 'react'
-import { useDispatch } from 'react-redux';
-import { addTodo, handleEditSubmit } from '../redux/todoapp/actions';
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo, handleEditSubmit } from "../redux/todoapp/actions";
 
-export const Form = ({editFormVisibility, editTodo, cancelUpdate}) => {
-
+export const Form = ({ editFormVisibility, editTodo, cancelUpdate }) => {
   // dispatch function to dispatch an action
   const dispatch = useDispatch();
 
   // todo value state for normal add todo form
-  const [todoValue, setTodoValue]=useState('');
+  const [todoValue, setTodoValue] = useState("");
 
   // state for if someone changes the (to edit) value in update form
-  const [editValue, setEditValue]=useState('');
+  const [editValue, setEditValue] = useState("");
 
   // useEffect is to show the (to edit) value in update form
-  useEffect(()=>{
+  useEffect(() => {
     setEditValue(editTodo.todo);
-  },[editTodo])
+  }, [editTodo]);
 
   // normal add todo submit
-  const handleSubmit=(e)=>{
-      e.preventDefault();
-      let date = new Date();
-      let time = date.getTime();
-      let todoObj={
-          id: time,
-          todo: todoValue,
-          completed: false
-      }
-      setTodoValue('');
-      dispatch(addTodo(todoObj))
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let date = new Date();
+    let time = date.getTime();
+    let todoObj = {
+      id: time,
+      todo: todoValue,
+      completed: false,
+    };
+    setTodoValue("");
+    dispatch(addTodo(todoObj));
+  };
 
   // update form submit
-  const editSubmit = (e) =>{
+  const editSubmit = (e) => {
     e.preventDefault();
-    let editedObj={
+    let editedObj = {
       id: editTodo.id,
       todo: editValue,
-      completed: false
-    }
-    dispatch(handleEditSubmit(editedObj))
-  }
+      completed: false,
+    };
+    dispatch(handleEditSubmit(editedObj));
+  };
 
   return (
     <>
-      {editFormVisibility===false?(
-        <form className='form-group custom-form' onSubmit={handleSubmit}>
-          <label>Add your todo-items</label>
-          <div className='input-and-btn'>
-              <input type="text" className='form-control' required
-              value={todoValue} onChange={(e)=>setTodoValue(e.target.value)}/>
-              <button type="submit" className='btn btn-secondary btn-md'>ADD</button>
+      {editFormVisibility === false ? (
+        <form className="form-group custom-form" onSubmit={handleSubmit}>
+          <label className="p-4">Add your todo-items</label>
+          <div className="input-and-btn">
+            <input
+              type="text"
+              className="form-control"
+              required
+              value={todoValue}
+              onChange={(e) => setTodoValue(e.target.value)}
+            />
+            <button type="submit" className="btn btn-secondary btn-md space">
+              ADD
+            </button>
           </div>
         </form>
-      ):(
-        <form className='form-group custom-form' onSubmit={editSubmit}>
-          <label>Update your todo-items</label>
-          <div className='input-and-btn'>
-              <input type="text" className='form-control' required
-              value={editValue||""} onChange={(e)=>setEditValue(e.target.value)}/>
-              <button type="submit" className='btn btn-secondary btn-md'>UPDATE</button>
+      ) : (
+        <form className="form-group custom-form" onSubmit={editSubmit}>
+          <label className="p-4">Update your todo-items</label>
+          <div className="input-and-btn">
+            <input
+              type="text"
+              className="form-control"
+              required
+              value={editValue || ""}
+              onChange={(e) => setEditValue(e.target.value)}
+            />
+            <button type="submit" className="btn btn-secondary btn-md space">
+              UPDATE
+            </button>
           </div>
-          <button type="button" className='btn btn-primary btn-md back-btn'
-          onClick={cancelUpdate}>BACK</button>
+          <button
+            type="button"
+            className="btn btn-primary btn-md back-btn"
+            onClick={cancelUpdate}
+          >
+            BACK
+          </button>
         </form>
       )}
     </>
-  )
-}
+  );
+};
